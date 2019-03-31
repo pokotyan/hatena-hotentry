b62b6e7cb398441b451dd16362e2565c5e324675
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2019-03-31 10:37:47 +0900

    vue add @vue/pwa

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import App from './App.vue'
+import './registerServiceWorker'
 
 Vue.use(Buefy);
 Vue.config.productionTip = false
